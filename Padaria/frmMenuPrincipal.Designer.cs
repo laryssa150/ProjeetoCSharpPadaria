e1c8fc505870ae5618a8613bba710276afddb335
LARYSSA FERREIRA DA SILVA <laryssa.fsilva5@senacsp.edu.br>
2025-03-19 11:55:33 -0300

    projetos padaria

---
+++ Padaria/frmMenuPrincipal.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace Padaria
+{
+    partial class frmMenuPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMenuPrincipal));
+            this.btnclientes = new System.Windows.Forms.Button();
+            this.btnVoltar = new System.Windows.Forms.Button();
+            this.btnComandas = new System.Windows.Forms.Button();
+            this.btnProdutos = new System.Windows.Forms.Button();
+            this.btnFornecedores = new System.Windows.Forms.Button();
+            this.btnCaixas = new System.Windows.Forms.Button();
+            this.btnUsuarios = new System.Windows.Forms.Button();
+            this.btnFuncionarios = new System.Windows.Forms.Button();
+            this.btnRelatórios = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnclientes
+            // 
+            this.btnclientes.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnclientes.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnclientes.Image = ((System.Drawing.Image)(resources.GetObject("btnclientes.Image")));
+            this.btnclientes.Location = new System.Drawing.Point(12, 12);
+            this.btnclientes.Name = "btnclientes";
+            this.btnclientes.Size = new System.Drawing.Size(164, 230);
+            this.btnclientes.TabIndex = 0;
+            this.btnclientes.Text = "Clientes";
+            this.btnclientes.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnclientes.TextImageRelation = System.Windows.Forms.TextImageRelation.TextAboveImage;
+            this.btnclientes.UseVisualStyleBackColor = true;
+            // 
+            // btnVoltar
+            // 
+            this.btnVoltar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnVoltar.Image = ((System.Drawing.Image)(resources.GetObject("btnVoltar.Image")));
+            this.btnVoltar.Location = new System.Drawing.Point(662, 500);
+            this.btnVoltar.Name = "btnVoltar";
+            this.btnVoltar.Size = new System.Drawing.Size(110, 49);
+            this.btnVoltar.TabIndex = 1;
+            this.btnVoltar.Text = "Voltar";
+            this.btnVoltar.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnVoltar.UseVisualStyleBackColor = true;
+            // 
+            // btnComandas
+            // 
+            this.btnComandas.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnComandas.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnComandas.Image = ((System.Drawing.Image)(resources.GetObject("btnComandas.Image")));
+            this.btnComandas.Location = new System.Drawing.Point(202, 12);
+            this.btnComandas.Name = "btnComandas";
+            this.btnComandas.Size = new System.Drawing.Size(164, 230);
+            this.btnComandas.TabIndex = 2;
+            this.btnComandas.Text = "Comanda";
+            this.btnComandas.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnComandas.TextImageRelation = System.Windows.Forms.TextImageRelation.TextAboveImage;
+            this.btnComandas.UseVisualStyleBackColor = true;
+            // 
+            // btnProdutos
+            // 
+            this.btnProdutos.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnProdutos.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnProdutos.Image = ((System.Drawing.Image)(resources.GetObject("btnProdutos.Image")));
+            this.btnProdutos.Location = new System.Drawing.Point(397, 12);
+            this.btnProdutos.Name = "btnProdutos";
+            this.btnProdutos.Size = new System.Drawing.Size(164, 230);
+            this.btnProdutos.TabIndex = 3;
+            this.btnProdutos.Text = "Produtos";
+            this.btnProdutos.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnProdutos.TextImageRelation = System.Windows.Forms.TextImageRelation.TextAboveImage;
+            this.btnProdutos.UseVisualStyleBackColor = true;
+            // 
+            // btnFornecedores
+            // 
+            this.btnFornecedores.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnFornecedores.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFornecedores.Image = ((System.Drawing.Image)(resources.GetObject("btnFornecedores.Image")));
+            this.btnFornecedores.Location = new System.Drawing.Point(590, 12);
+            this.btnFornecedores.Name = "btnFornecedores";
+            this.btnFornecedores.Size = new System.Drawing.Size(164, 230);
+            this.btnFornecedores.TabIndex = 4;
+            this.btnFornecedores.Text = "Forncedores";
+            this.btnFornecedores.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnFornecedores.TextImageRelation = System.Windows.Forms.TextImageRelation.TextAboveImage;
+            this.btnFornecedores.UseVisualStyleBackColor = true;
+            // 
+            // btnCaixas
+            // 
+            this.btnCaixas.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnCaixas.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCaixas.Image = ((System.Drawing.Image)(resources.GetObject("btnCaixas.Image")));
+            this.btnCaixas.Location = new System.Drawing.Point(12, 268);
+            this.btnCaixas.Name = "btnCaixas";
+            this.btnCaixas.Size = new System.Drawing.Size(164, 230);
+            this.btnCaixas.TabIndex = 5;
+            this.btnCaixas.Text = "Caixa";
+            this.btnCaixas.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnCaixas.TextImageRelation = System.Windows.Forms.TextImageRelation.TextAboveImage;
+            this.btnCaixas.UseVisualStyleBackColor = true;
+            // 
+            // btnUsuarios
+            // 
+            this.btnUsuarios.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnUsuarios.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUsuarios.Image = ((System.Drawing.Image)(resources.GetObject("btnUsuarios.Image")));
+            this.btnUsuarios.Location = new System.Drawing.Point(202, 268);
+            this.btnUsuarios.Name = "btnUsuarios";
+            this.btnUsuarios.Size = new System.Drawing.Size(164, 230);
+            this.btnUsuarios.TabIndex = 6;
+            this.btnUsuarios.Text = "Usuários";
+            this.btnUsuarios.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnUsuarios.TextImageRelation = System.Windows.Forms.TextImageRelation.TextAboveImage;
+            this.btnUsuarios.UseVisualStyleBackColor = true;
+            // 
+            // btnFuncionarios
+            // 
+            this.btnFuncionarios.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnFuncionarios.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFuncionarios.Image = ((System.Drawing.Image)(resources.GetObject("btnFuncionarios.Image")));
+            this.btnFuncionarios.Location = new System.Drawing.Point(397, 268);
+            this.btnFuncionarios.Name = "btnFuncionarios";
+            this.btnFuncionarios.Size = new System.Drawing.Size(164, 230);
+            this.btnFuncionarios.TabIndex = 7;
+            this.btnFuncionarios.Text = "Funcionários";
+            this.btnFuncionarios.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnFuncionarios.TextImageRelation = System.Windows.Forms.TextImageRelation.TextAboveImage;
+            this.btnFuncionarios.UseVisualStyleBackColor = true;
+            this.btnFuncionarios.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // btnRelatórios
+            // 
+            this.btnRelatórios.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnRelatórios.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRelatórios.Image = ((System.Drawing.Image)(resources.GetObject("btnRelatórios.Image")));
+            this.btnRelatórios.Location = new System.Drawing.Point(590, 264);
+            this.btnRelatórios.Name = "btnRelatórios";
+            this.btnRelatórios.Size = new System.Drawing.Size(164, 230);
+            this.btnRelatórios.TabIndex = 8;
+            this.btnRelatórios.Text = "Relatórios";
+            this.btnRelatórios.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnRelatórios.TextImageRelation = System.Windows.Forms.TextImageRelation.TextAboveImage;
+            this.btnRelatórios.UseVisualStyleBackColor = true;
+            // 
+            // frmMenuPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.btnRelatórios);
+            this.Controls.Add(this.btnFuncionarios);
+            this.Controls.Add(this.btnUsuarios);
+            this.Controls.Add(this.btnCaixas);
+            this.Controls.Add(this.btnFornecedores);
+            this.Controls.Add(this.btnProdutos);
+            this.Controls.Add(this.btnComandas);
+            this.Controls.Add(this.btnVoltar);
+            this.Controls.Add(this.btnclientes);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "frmMenuPrincipal";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Padaria - Menu Principal";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnclientes;
+        private System.Windows.Forms.Button btnVoltar;
+        private System.Windows.Forms.Button btnComandas;
+        private System.Windows.Forms.Button btnProdutos;
+        private System.Windows.Forms.Button btnFornecedores;
+        private System.Windows.Forms.Button btnCaixas;
+        private System.Windows.Forms.Button btnUsuarios;
+        private System.Windows.Forms.Button btnFuncionarios;
+        private System.Windows.Forms.Button btnRelatórios;
+    }
+}
