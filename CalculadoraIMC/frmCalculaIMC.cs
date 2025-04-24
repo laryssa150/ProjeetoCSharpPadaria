9556145921997858903bc26ab6ef022c3b8b60b8
laryssa150 <ferreiralaryssa623@gmail.com>
2025-04-24 11:19:34 -0300

    Add files via upload

---
+++ CalculadoraIMC/frmCalculaIMC.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculadoraIMC
+{
+    public partial class frmCalculaIMC : Form
+    {
+        public frmCalculaIMC()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSair_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            mskAltura.Clear();
+            mskIdade.Clear();
+            mskPeso.Clear();
+
+            mskIdade.Focus();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            double imc = 0, peso, altura;
+
+            try
+            {
+
+                peso = Convert.ToDouble(mskPeso.Text);
+                altura = Convert.ToDouble(mskAltura.Text);
+
+                imc = peso / (altura * altura);
+
+                
+
+                txtResultado.Text = imc.ToString();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Favor inserir valores válidos.",
+                    "Mensagem do sistema",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error,
+                    MessageBoxDefaultButton.Button1);
+                
+                mskAltura.Clear();
+                mskIdade.Clear();
+                mskPeso.Clear();
+                mskIdade.Focus();
+            }
+        }
+
+        private void frmCalculaIMC_Load(object sender, EventArgs e)
+        {
+            rdbFeminino.Checked = false;
+            rdbMasculino.Checked = false;
+        }
+    }
+}
